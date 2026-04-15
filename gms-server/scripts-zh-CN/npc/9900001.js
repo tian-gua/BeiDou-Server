07cc59aeea22f726a8882b5f66a611886dfadc39
yehao <77729052@qq.com>
2026-04-15 17:27:21 +0800

    mod: 护肝

--- gms-server/scripts-zh-CN/npc/9900001.js
+++ gms-server/scripts-zh-CN/npc/9900001.js
@@ -49,6 +49,10 @@ function action(mode, type, selection) {
         text += "#L0#新人福利#l \t #L1#每日签到#l \t #L2#在线奖励#l\r\n";
         text += "#L3#传送自由#l \t #L4#爆率一览#l \t #L5#超级传送#l\r\n";
         text += "#L6#删除道具#l \t #L8#技能全满#l \t #L7#学二段跳#l\r\n";
+        text += "#L9#快捷商店#l\r\n";
+        if (cm.getPlayer().getName() === "melon") {
+            text += "#L98#开启吸怪#l \t #L99#关闭吸怪#l\r\n";
+        }
         if (cm.getPlayer().isGM()) {
             text += "\r\n\r\n";
             text += "\t\t\t\t#r=====以下内容仅GM可见=====\r\n";
@@ -90,10 +94,25 @@ function doSelect(selection) {
             openNpc("一键删除道具");
             break;
         case 7:
-            cm.new_keyboard_skill(57, 1, 14101004)
+            cm.newKeyboardSkill(57, 1, 14101004)
+            cm.dispose();
             break;
         case 8:
-            cm.max_skill(57, 1, 14101004)
+            cm.maxSkill()
+            cm.dispose();
+            break;
+        case 9:
+            cm.dispose();
+            cm.openShopNPC(9090000);
+            cm.dispose();
+            break;
+        case 98:
+            cm.mobVac(true);
+            cm.dispose();
+            break;
+        case 99:
+            cm.mobVac(false);
+            cm.dispose();
             break;
         // GM功能
         case 61:
